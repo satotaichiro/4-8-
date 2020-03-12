e21f3c762673c1f1f62d87f9735d58b4824a2698
satotaichiro <tapoya09@gmail.com>
2020-03-12 21:16:10 +0900

    add[4-8]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,18 +4,39 @@ class BooksController < ApplicationController
   	@book = Book.new
   end
 
+  def top
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path
+  end
+
   def show
+  	@book = Book.find(params[:id])
   end
 
   def create
   	@book = Book.new(book_params)
   if @book.save
-  	redirect_to '/books'
+  	flash[:notice] = 'Book was successfully created.'
+  	redirect_to book_path(@book.id)
   else
   	@books = Book.all
   	render action: :index
   end
 end
+
+  def edit
+ 	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	book.update(book_params)
+  	redirect_to book_path(book)
+  end
 
   private
   def book_params
